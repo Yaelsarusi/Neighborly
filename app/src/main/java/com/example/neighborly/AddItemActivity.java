ec311a0128ddcd1090fb271dbf1b4302f8e8c222
daphnaleventer <daphna.leventer@huji.mail.ac.il>
2019-12-23 18:24:10 +0200

    Add all activities.

---
+++ app/src/main/java/com/example/neighborly/AddItemActivity.java
@@ -0,0 +1,14 @@
+package com.example.neighborly;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class AddItemActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add_item);
+    }
+}
---
+++ app/src/main/java/com/example/neighborly/JoinBuildingActivity.java
@@ -0,0 +1,14 @@
+package com.example.neighborly;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class JoinBuildingActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_join_building);
+    }
+}
