ba7073f50723df3403a4e78277ca46cf4987e261
daphnaleventer <daphna.leventer@huji.mail.ac.il>
2019-12-30 15:38:46 +0200

    Create a nevigation bar for the feed, profile and neighbours.

--- app/src/main/java/com/example/neighborly/MainActivity.java
+++ app/src/main/java/com/example/neighborly/MainActivity.java
@@ -1,8 +1,15 @@
 package com.example.neighborly;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
 
+import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.MenuItem;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +17,38 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        BottomNavigationView bottomNavigationView = findViewById(R.id.bottom_navigation);
+        bottomNavigationView.setOnNavigationItemSelectedListener(listener);
+
+        //The if statement is to prevent initializing when the device is rotated
+        if (savedInstanceState == null) {
+            getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,
+                    new FeedFragment()).commit();
+        }
     }
+
+    private BottomNavigationView.OnNavigationItemSelectedListener listener = new BottomNavigationView.OnNavigationItemSelectedListener() {
+        @Override
+        public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
+            Fragment selected = null;
+
+            switch (menuItem.getItemId()){
+                case R.id.nev_feed:
+                    selected = new FeedFragment();
+                    break;
+                case R.id.nev_profile:
+                    selected = new PrifileFragment();
+                    break;
+                case R.id.nev_neighbors:
+                    selected = new NeighboursFragment();
+                    break;
+            }
+            getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,
+                    selected).commit();
+            return true;
+        }
+    };
+
+
 }
