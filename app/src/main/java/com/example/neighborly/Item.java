96081b784241cf6e3dce34d77c2cdc46176f163f
shakedweitz <shaked.weitz@mail.huji.ac.il>
2020-01-06 14:01:26 +0200

    pr fixes

--- app/src/main/java/com/example/neighborly/Item.java
+++ app/src/main/java/com/example/neighborly/Item.java
@@ -8,10 +8,14 @@ public class Item {
 
     private Bitmap image;
     private String name;
-    private FirebaseUser owner;
+    private String owner;
     private String description;
 
-    public Item(Bitmap image, String name, FirebaseUser owner, String description) {
+    public Item()
+    {
+    }
+
+    public Item(Bitmap image, String name, String owner, String description) {
         this.image = image;
         this.name = name;
         this.owner = owner;
@@ -34,11 +38,11 @@ public class Item {
         this.name = name;
     }
 
-    public FirebaseUser getOwner() {
+    public String getOwner() {
         return owner;
     }
 
-    public void setOwner(FirebaseUser owner) {
+    public void setOwner(String owner) {
         this.owner = owner;
     }
 
